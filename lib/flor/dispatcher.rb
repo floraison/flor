16cb86a4a8ebd726611bd3b17dcc1018cc5c1f22
John Mettraux <jmettraux@gmail.com>
2016-01-01 16:33:42 +0900

    2016

--- lib/flor/dispatcher.rb
+++ lib/flor/dispatcher.rb
@@ -1,6 +1,6 @@
 
 #--
-# Copyright (c) 2015-2015, John Mettraux, jmettraux+flon@gmail.com
+# Copyright (c) 2015-2016, John Mettraux, jmettraux+flon@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
