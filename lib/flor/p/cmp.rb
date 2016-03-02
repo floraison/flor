d199cf6b72ed29ca659ad279d115b4736f561e7d
John Mettraux <jmettraux@gmail.com>
2016-03-02 21:05:55 +0900

    implement string comparison

---
+++ lib/flor/p/cmp.rb
@@ -0,0 +1,70 @@
+#--
+# Copyright (c) 2015-2016, John Mettraux, jmettraux+flon@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+# Made in Japan.
+#++
+
+
+class Flor::Pro::Cmp < Flor::Procedure
+
+  names %w[ = ]
+
+  FULL_NAMES = { '=' => :eq }
+
+  def execute
+
+    @node['rets'] = []
+
+    receive
+  end
+
+  def receive
+
+    ms = sequence_receive
+
+    m = ms.size == 1 && ms.first
+    #
+    if m['point'] == 'receive' && m['nid'] == parent
+
+      payload['ret'] =
+        if @node['rets'].empty?
+          true
+        else
+          send(
+            "#{@node['rets'].first.class.to_s.downcase}_#{FULL_NAMES[tree[0]]}",
+            @node['rets'])
+        end
+    end
+
+    ms
+  end
+
+  protected
+
+  def string_eq(rets)
+
+    first = rets[0]
+    rets[1..-1].each { |e| return false if e != first }
+
+    true
+  end
+end
+
---
+++ spec/p/cmp_spec.rb
@@ -0,0 +1,42 @@
+
+#
+# specifying flor
+#
+# Wed Mar  2 20:44:53 JST 2016
+#
+
+require 'spec_helper'
+
+
+describe 'Flor procedures' do
+
+  before :each do
+
+    @executor = Flor::TransientExecutor.new
+  end
+
+  describe '=' do
+
+    it 'compares strings' do
+
+      rad = %{
+        sequence
+          push f.l
+            =
+              val "alpha"
+              val alpha
+          push f.l
+            =
+              val "alpha"
+              val "bravo"
+      }
+
+      r = @executor.launch(rad, payload: { 'l' => [] })
+
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq(false)
+      expect(r['payload']['l']).to eq([ true, false ])
+    end
+  end
+end
+
