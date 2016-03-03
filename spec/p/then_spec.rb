7e6c350a1f6ba77542d4987ab326e859aa0f6588
John Mettraux <jmettraux@gmail.com>
2016-03-04 07:06:15 +0900

    implement "then" and "else"

--- spec/p/then_spec.rb
+++ spec/p/then_spec.rb
@@ -23,28 +23,74 @@ describe 'Flor procedures' do
         sequence
           true
           then
-            push f.l
-              1
-            push f.l
-              2
-          push l
-            3
+            push f.l 1
+            push f.l 2
+          push f.l 3
       }
 
       r = @executor.launch(rad, payload: { 'l' => [] })
 
       expect(r['point']).to eq('terminated')
-      expect(r['payload']['ret']).to eq(7)
+      expect(r['payload']['ret']).to eq(3)
       expect(r['payload']['l']).to eq([ 1, 2, 3 ])
     end
 
-    it 'does not run its children if $(f.ret) is false'
+    it 'does not run its children if $(f.ret) is not true' do
+
+      rad = %{
+        sequence
+          1
+          then
+            push f.l 1
+            push f.l 2
+          push f.l 3
+      }
+
+      r = @executor.launch(rad, payload: { 'l' => [] })
+
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq(3)
+      expect(r['payload']['l']).to eq([ 3 ])
+    end
   end
 
   describe 'else' do
 
-    it 'runs its children if $(f.ret) is false'
-    it 'does not run its children if $(f.ret) is true'
+    it 'runs its children if $(f.ret) is false' do
+
+      rad = %{
+        sequence
+          false
+          else
+            push f.l 1
+            push f.l 2
+          push f.l 3
+      }
+
+      r = @executor.launch(rad, payload: { 'l' => [] })
+
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq(3)
+      expect(r['payload']['l']).to eq([ 1, 2, 3 ])
+    end
+
+    it 'does not run its children if $(f.ret) is not false' do
+
+      rad = %{
+        sequence
+          1
+          else
+            push f.l 1
+            push f.l 2
+          push f.l 3
+      }
+
+      r = @executor.launch(rad, payload: { 'l' => [] })
+
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq(3)
+      expect(r['payload']['l']).to eq([ 3 ])
+    end
   end
 end
 
