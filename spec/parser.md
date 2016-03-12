faa8eca30dc12e32efa4050c0563594cc2b33e5a
John Mettraux <jmettraux@gmail.com>
2016-03-12 18:01:01 +0900

    use literate specs for parser.rb

---
+++ spec/parser.md
@@ -0,0 +1,200 @@
+
+<!--
+# specifying flor
+#
+# Mon Mar  7 06:24:41 JST 2016
+-->
+
+# atoms
+
+```radial
+  1
+```
+parses to
+```ruby
+  [ '_num', 1, 1 ]
+```
+
+```radial
+  11.01
+```
+parses to
+```ruby
+  [ '_num', 11.01, 1 ]
+```
+
+```radial
+  true
+```
+parses to
+```ruby
+  [ '_boo', true, 1 ]
+```
+
+```radial
+  false
+```
+parses to
+```ruby
+  [ '_boo', false, 1 ]
+```
+
+```radial
+  null
+```
+parses to
+```ruby
+  [ '_nul', nil, 1 ]
+```
+
+```radial
+  abc
+```
+parses to
+```ruby
+  [ 'abc', [], 1 ]
+```
+
+```radial
+  'def'
+```
+parses to
+```ruby
+  [ '_sqs', 'def', 1 ]
+```
+
+```radial
+  "ghi"
+```
+parses to
+```ruby
+  [ '_dqs', 'ghi', 1 ]
+```
+
+```radial
+  /jkl/i
+```
+parses to
+```ruby
+  [ '_rxs', '/jkl/i', 1 ]
+```
+
+```radial
+  [ 1, 2, 3 ]
+```
+parses to
+```ruby
+    [ '_arr', [
+    [ '_num', 1, 1 ], [ '_num', 2, 1 ], [ '_num', 3, 1 ]
+  ], 1 ]
+```
+
+```radial
+  { a: A, b: 2, c: true }
+```
+parses to
+```ruby
+  [ '_obj', [
+    [ 'a', [], 1 ], [ 'A', [], 1 ],
+    [ 'b', [], 1 ], [ '_num', 2, 1 ],
+    [ 'c', [], 1 ], [ '_boo', true, 1 ]
+  ], 1 ]
+```
+
+# operations
+
+```radial
+  10 + 11 - 5
+```
+parses to
+```ruby
+  [ '-', [
+    [ '+', [
+      [ '_num', 10, 1 ],
+      [ '_num', 11, 1 ]
+    ], 1 ],
+    [ '_num', 5, 1 ]
+  ], 1 ]
+```
+
+```radial
+  1 + 1 * 2
+```
+parses to
+```ruby
+  [ '+', [
+    [ '_num', 1, 1 ],
+    [ '*', [
+      [ '_num', 1, 1 ],
+      [ '_num', 2, 1 ]
+    ], 1 ]
+  ], 1 ]
+```
+
+# lines
+
+```radial
+  sequence
+```
+parses to
+```ruby
+  [ 'sequence', [], 1 ]
+```
+
+```radial
+  sequence
+    a
+    b
+```
+parses to
+```ruby
+  [ 'sequence', [
+    [ 'a', [], 2 ],
+    [ 'b', [], 3 ]
+  ], 1 ]
+```
+
+```radial
+  sequence a b
+```
+parses to
+```ruby
+  [ 'sequence', [
+    [ 'a', [], 1 ],
+    [ 'b', [], 1 ]
+  ], 1 ]
+```
+
+```radial
+  sequence a, vars: 1, timeout: 1h, b
+```
+parses to
+```ruby
+  [ 'sequence', [
+    [ '_atts', [
+      [ 'vars', [], 1 ], [ '_num', 1, 1 ],
+      [ 'timeout', [], 1 ], [ '1h', [], 1 ]
+    ], 1 ],
+    [ 'a', [], 1 ],
+    [ 'b', [], 1 ]
+  ], 1 ]
+```
+
+
+```radial
+  sequence a: 1 + 1, 2
+```
+parses to
+```ruby
+  [ 'sequence', [
+    [ '_atts', [
+      [ 'a', [], 1 ],
+      [ '+', [
+        [ '_num', 1, 1 ],
+        [ '_num', 1, 1 ],
+      ], 1 ]
+    ], 1 ],
+    [ '_num', 2, 1 ]
+  ], 1 ]
+```
+
--- spec/parser.rb
+++ spec/parser.rb
@@ -12,190 +12,42 @@ require 'flor/parser'
 
 describe Flor::Rad do
 
-  context 'atoms' do
+  lines = File.readlines(File.join(File.dirname(__FILE__), 'parser.md'))
 
-    [
-      [
-        __LINE__,
-        %{ 1 },
-        [ '_num', 1, 1 ]
-      ],
-      [
-        __LINE__,
-        %{ 11.01 },
-        [ '_num', 11.01, 1 ]
-      ],
-      [
-        __LINE__,
-        %{ true },
-        [ '_boo', true, 1 ]
-      ],
-      [
-        __LINE__,
-        %{ false },
-        [ '_boo', false, 1 ]
-      ],
-      [
-        __LINE__,
-        %{ null },
-        [ '_nul', nil, 1 ]
-      ],
-      [
-        __LINE__,
-        %{ abc },
-        [ 'abc', [], 1 ]
-      ],
-      [
-        __LINE__,
-        %{ 'def' },
-        [ '_sqs', 'def', 1 ]
-      ],
-      [
-        __LINE__,
-        %{ "ghi" },
-        [ '_dqs', 'ghi', 1 ]
-      ],
-      [
-        __LINE__,
-        %{ /jkl/i },
-        [ '_rxs', '/jkl/i', 1 ]
-      ],
-      [
-        __LINE__,
-        %{ [ 1, 2, 3 ] },
-        [ '_arr', [
-          [ '_num', 1, 1 ], [ '_num', 2, 1 ], [ '_num', 3, 1 ]
-        ], 1 ]
-      ],
-      [
-        __LINE__,
-        %{ { a: A, b: 2, c: true } },
-        [ '_obj', [
-          [ 'a', [], 1 ], [ 'A', [], 1 ],
-          [ 'b', [], 1 ], [ '_num', 2, 1 ],
-          [ 'c', [], 1 ], [ '_boo', true, 1 ]
-        ], 1 ]
-      ],
-    ].each { |li, ra, tr|
+  mod = :out
+  rad = []
+  rub = []
 
-      rad = ra.strip.gsub(/\n/, '\n').gsub(/ +/, ' ')
-      rad = "#{rad[0, 60]}..." if rad.length > 60
-      title = "parses li#{li} `#{rad}`"
+  lines.each_with_index do |line, i|
 
-      it(title) { expect(Flor::Rad.parse(ra)).to eq(tr) }
-    }
-  end
+    if mod == :out && line == "```ruby\n"
 
-  context 'operations' do
+      mod = :ruby
 
-    [
-      [
-        __LINE__,
-        %{
-          10 + 11 - 5
-        },
-        [ '-', [
-          [ '+', [
-            [ '_num', 10, 2 ],
-            [ '_num', 11, 2 ]
-          ], 2 ],
-          [ '_num', 5, 2 ]
-        ], 2 ]
-      ],
-      [
-        __LINE__,
-        %{
-          1 + 1 * 2
-        },
-        [ '+', [
-          [ '_num', 1, 2 ],
-          [ '*', [
-            [ '_num', 1, 2 ],
-            [ '_num', 2, 2 ]
-          ], 2 ]
-        ], 2 ]
-      ],
-    ].each { |li, ra, tr|
+    elsif mod == :out && line == "```radial\n"
 
-      rad = ra.strip.gsub(/\n/, '\n').gsub(/ +/, ' ')
-      rad = "#{rad[0, 60]}..." if rad.length > 60
-      title = "parses li#{li} `#{rad}`"
+      mod = :radial
 
-      it(title) { expect(Flor::Rad.parse(ra)).to eq(tr) }
-    }
-  end
+    elsif line == "```\n"
 
-  context 'basic' do
+      if mod == :ruby
 
-    [
-      [
-        __LINE__,
-        %{
-          sequence
-        },
-        [ 'sequence', [], 2 ]
-      ],
-      [
-        __LINE__,
-        %{
-          sequence
-            a
-            b
-        },
-        [ 'sequence', [
-          [ 'a', [], 3 ],
-          [ 'b', [], 4 ]
-        ], 2 ]
-      ],
-      [
-        __LINE__,
-        %{
-          sequence a b
-        },
-        [ 'sequence', [
-          [ 'a', [], 2 ],
-          [ 'b', [], 2 ]
-        ], 2 ]
-      ],
-      [
-        __LINE__,
-        %{
-          sequence a, vars: 1, timeout: 1h, b
-        },
-        [ 'sequence', [
-          [ '_atts', [
-            [ 'vars', [], 2 ], [ '_num', 1, 2 ],
-            [ 'timeout', [], 2 ], [ '1h', [], 2 ]
-          ], 2 ],
-          [ 'a', [], 2 ],
-          [ 'b', [], 2 ]
-        ], 2 ]
-      ],
-      [
-        __LINE__,
-        %{
-          sequence a: 1 + 1, 2
-        },
-        [ 'sequence', [
-          [ '_atts', [
-            [ 'a', [], 2 ],
-            [ '+', [
-              [ '_num', 1, 2 ],
-              [ '_num', 1, 2 ],
-            ], 2 ]
-          ], 2 ],
-          [ '_num', 2, 2 ]
-        ], 2 ]
-      ],
+        rad = rad.join
+        ra = rad.strip.gsub(/\n/, '\n').gsub(/ +/, ' ')
+        ra = "#{tra[0, 60]}..." if ra.length > 60
+        title = "parses li#{i} `#{ra}`"
 
-    ].each { |li, ra, tr|
+        ru = Kernel.eval(rub.join)
 
-      rad = ra.strip.gsub(/\n/, '\n').gsub(/ +/, ' ')
-      rad = "#{rad[0, 60]}..." if rad.length > 60
-      title = "parses li#{li} `#{rad}`"
+        it(title) { expect(Flor::Rad.parse(ra)).to eq(ru) }
 
-      it(title) { expect(Flor::Rad.parse(ra)).to eq(tr) }
-    }
+        rub = []
+        rad = []
+      end
+      mod = :out
+    elsif mod != :out
+      (mod == :ruby ? rub : rad) << line
+    end
   end
 end
 
