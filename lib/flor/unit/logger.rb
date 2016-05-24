788c928bad1329290928df6b83d1e191f56af3d3
John Mettraux <jmettraux@gmail.com>
2016-05-25 06:21:07 +0900

    summarize blobs when logging sequel activity

--- lib/flor/unit/logger.rb
+++ lib/flor/unit/logger.rb
@@ -55,7 +55,11 @@ module Flor
 
       return unless @unit.conf['log_sto']
 
-      # TODO summarize content columns
+      # reminder: substitutes only one blob
+      m =
+        msg.match(/ (INSERT|UPDATE) .+ 0[xX]([a-fA-F0-9]+)/) || # ms-sql
+        msg.match(/ (INSERT|UPDATE) .+ [xX]'([a-fA-F0-9]+)'/)   # sqlite
+      msg = msg.sub(m[2], "#{m[2][0, 14]}(...len#{m[2].length})") if m
 
       puts "t#{Thread.current.hash} #{level.upcase} #{msg}"
     end
