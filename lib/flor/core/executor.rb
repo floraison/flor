4fa6147dff57dc95c0de2753e8a9336f8b40f321
John Mettraux <jmettraux@gmail.com>
2016-05-24 16:54:24 +0900

    inflate "ceased" messages

--- lib/flor/core/executor.rb
+++ lib/flor/core/executor.rb
@@ -157,10 +157,9 @@ module Flor
 
       node = @execution['nodes'][nid]
 
-      return [] unless node
-      #return [
-      #  message.merge('point' => 'ceased')
-      #] unless node
+      return [
+        message.merge('point' => 'ceased', 'nid' => from, 'from' => nil)
+      ] unless node
 
       apply(node, message)
     end
