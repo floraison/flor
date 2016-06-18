9e0f56cca64fa053a30087c3d8ec93bff9a8427b
John Mettraux <jmettraux@gmail.com>
2016-06-18 18:13:45 +0900

    write down test load tree in loader_spec.rb

--- spec/unit/loader_spec.rb
+++ spec/unit/loader_spec.rb
@@ -12,6 +12,45 @@ describe Flor::Loader do
 
   describe '#load' do
 
+    # spec/unit/loader
+    # ├── etc
+    # │   └── variables
+    # │       └── dot.json
+    # ├── lib
+    # │   ├── flows
+    # │   │   ├── net.example
+    # │   │   │   └── flow0.flon
+    # │   │   └── org.example
+    # │   │       └── flow0.flon
+    # │   └── taskers
+    # │       ├── alice
+    # │       │   └── dot.json
+    # │       ├── net.example
+    # │       └── org.example
+    # │           ├── alice
+    # │           │   └── dot.json
+    # │           └── bob
+    # │               └── dot.json
+    # └── usr
+    #     ├── net.example
+    #     │   ├── etc
+    #     │   │   └── variables
+    #     │   │       └── dot.json
+    #     │   └── lib
+    #     │       ├── flows
+    #     │       │   └── flow1.flon
+    #     │       └── taskers
+    #     │           └── bob
+    #     │               └── dot.json
+    #     └── org.example
+    #         ├── etc
+    #         │   └── variables
+    #         │       └── dot.json
+    #         └── lib
+    #             ├── flows
+    #             │   └── flow1.flon
+    #             └── taskers
+
     it 'loads'
   end
 end
