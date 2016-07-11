23c7b1bed032f712c73edd0769c67d44abb70537
John Mettraux <jmettraux@gmail.com>
2016-07-11 20:48:24 +0900

    ensure _obj results have string keys

--- lib/flor/pcore/_obj.rb
+++ lib/flor/pcore/_obj.rb
@@ -46,7 +46,10 @@ class Flor::Pro::Obj < Flor::Procedure
 
   def receive_last
 
-    payload_copy['ret'] = Hash[*@node['rets']]
+    payload_copy['ret'] =
+      @node['rets']
+        .each_slice(2)
+        .inject({}) { |h, (k, v)| h[k.to_s] = v; h }
 
     reply
   end
