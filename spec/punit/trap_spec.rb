95f0e1c0a457a6dfd7208dfdcf8be78bb30aa6a9
John Mettraux <jmettraux@gmail.com>
2016-08-28 21:20:32 +0900

    refine "trap" heap: spec (take 2)

--- spec/punit/trap_spec.rb
+++ spec/punit/trap_spec.rb
@@ -191,10 +191,10 @@ describe 'Flor punit' do
             .each_with_index
             .collect { |t, i| "#{i}:#{t.text}" }.join("\n")
         ).to eq(%w{
-          0:-0
-          1:sequence-0_1
-          2:-0_1
-          3:-0
+          0:receive--0
+          1:execute-sequence-0_1
+          2:receive--0_1
+          3:receive--0
         }.collect(&:strip).join("\n"))
       end
     end
