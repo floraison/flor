20efe1d0f298acc2e9dbaf92a1bac559a0f514c3
John Mettraux <jmettraux@gmail.com>
2016-12-19 04:58:57 +0900

    Scaffold basic repl commands

--- lib/flor/tools/repl.rb
+++ lib/flor/tools/repl.rb
@@ -19,11 +19,24 @@ module Flor::Tools
       end
       unit.start
 
+      @lines = []
+      @payload = {}
+      @vars = {}
+
       loop do
 
         $stdout.print("flor> ")
-        line = ($stdin.readline rescue false); break unless line
-        p line
+        line = ($stdin.readline rescue false)
+
+        break unless line
+        next if line.strip == ''
+
+        cmd = "cmd_#{line.split(/\s/).first}".to_sym
+        if cmd.size > 4 && respond_to?(cmd)
+          send(cmd, line)
+        else
+          @lines << line
+        end
       end
 
       $stdout.puts
@@ -31,9 +44,40 @@ module Flor::Tools
 
     protected
 
-    def cmd_launch
+    def cmd_launch(line)
+
+puts "launched..."
     end
-    def cmd_help
+    alias cmd_run cmd_launch
+
+    def cmd_help(line)
+
+puts "help..."
+    end
+
+    def cmd_exit(line)
+
+      exit(0)
+    end
+
+    def cmd_list(line)
+
+      w = [ 2, @lines.size.to_s.length ].max
+
+      @lines.each_with_index do |l, i|
+        puts "%0#{w}i  %s" % [ i, l ]
+      end
+    end
+
+    def cmd_new(line)
+# TODO erase lines, payload and vars
+    end
+    def cmd_save(line)
+# TODO save lines (and payload and vars) to a file
+    end
+
+    def cmd_cont(line)
+# TODO eventually, resume the current execution
     end
   end
 end
