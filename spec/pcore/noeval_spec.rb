439d4ebc050b755b866634debff1b6cd09be026e
John Mettraux <jmettraux@gmail.com>
2016-12-24 20:33:47 +0900

    Implement "noeval"

---
+++ spec/pcore/noeval_spec.rb
@@ -0,0 +1,49 @@
+
+#
+# specifying flor
+#
+# Sat Dec 24 20:31:59 JST 2016
+#
+
+require 'spec_helper'
+
+
+describe 'Flor procedures' do
+
+  before :each do
+
+    @executor = Flor::TransientExecutor.new
+  end
+
+  describe 'noeval' do
+
+    it 'leaves f.ret as is' do
+
+      flon = %{
+        sequence
+          1
+          noeval _
+      }
+
+      r = @executor.launch(flon)
+
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq(1)
+    end
+
+    it "doesn't let its children getting evaluated" do
+
+      flon = %{
+        set a 1
+        noeval
+          set a 2
+      }
+
+      r = @executor.launch(flon)
+
+      expect(r['point']).to eq('terminated')
+      expect(r['vars']['a']).to eq(1)
+    end
+  end
+end
+
