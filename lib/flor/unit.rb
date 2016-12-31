bec81a3267718f269c95f7a1f8e522981bf13cc4
John Mettraux <jmettraux@gmail.com>
2017-01-01 07:15:57 +0900

    2017
    [ci skip]

--- lib/flor/unit.rb
+++ lib/flor/unit.rb
@@ -1,5 +1,5 @@
 #--
-# Copyright (c) 2015-2016, John Mettraux, jmettraux+flon@gmail.com
+# Copyright (c) 2015-2017, John Mettraux, jmettraux+flor@gmail.com
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
