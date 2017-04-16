4655c86460af26dad788a5a033d48caa1156ee35
John Mettraux <jmettraux@gmail.com>
2017-04-17 06:06:09 +0900

    Introduce "echo"

---
+++ lib/flor/pcore/echo.rb
@@ -0,0 +1,23 @@
+
+class Flor::Pro::Echo < Flor::Procedure
+
+  name 'echo'
+
+  def receive_last_att
+
+    case ret = payload['ret']
+    when String then puts(ret)
+    else pp(ret)
+    end
+
+    super
+  end
+
+  def receive_last
+
+    payload['ret'] = node_payload_ret
+
+    wrap
+  end
+end
+
