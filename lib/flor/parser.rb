9c5f9881d86233d25fc5042e404233754be2b883
John Mettraux <jmettraux@gmail.com>
2017-05-07 09:02:29 +0900

    Remove unused type in Flor::Lang::Nod

--- lib/flor/parser.rb
+++ lib/flor/parser.rb
@@ -154,7 +154,7 @@ module Flor
 
     def rewrite_par(t)
 
-      Nod.new(t.name, t.lookup(:node), nil).to_a
+      Nod.new(t.lookup(:node), nil).to_a
     end
 
     def rewrite_ref(t); [ t.string, [], ln(t) ]; end
@@ -240,9 +240,8 @@ module Flor
       attr_accessor :parent, :indent
       attr_reader :type, :children
 
-      def initialize(type, tree, outdent)
+      def initialize(tree, outdent)
 
-        @type = type
         @parent = nil
         @indent = -1
         @head = 'sequence'
@@ -370,12 +369,12 @@ module Flor
 
     def rewrite_flor(t)
 
-      prev = root = Nod.new(t.name, nil, nil)
+      prev = root = Nod.new(nil, nil)
 
       t.gather(:line).each do |lt|
         nt = lt.lookup(:node); next unless nt
         ot = lt.children.last.string
-        n = Nod.new(nt.name, nt, ot)
+        n = Nod.new(nt, ot)
         prev.append(n)
         prev = n
       end
