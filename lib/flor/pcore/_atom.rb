76ddf330292509999d3dc1deae193f9d816b4c3e
John Mettraux <jmettraux@gmail.com>
2017-05-08 15:03:05 +0900

    Restyle case in _atom.rb

--- lib/flor/pcore/_atom.rb
+++ lib/flor/pcore/_atom.rb
@@ -7,11 +7,11 @@ class Flor::Pro::Atom < Flor::Procedure
 
     payload['ret'] =
       case @node['heat0']
-        when '_nul' then nil
-        when '_dqs' then expand(tree[1])
-        when '_rxs' then [ tree[0], expand(tree[1]), *tree[2..-1] ]
-        when '_func' then tree
-        else tree[1]
+      when '_nul' then nil
+      when '_dqs' then expand(tree[1])
+      when '_rxs' then [ tree[0], expand(tree[1]), *tree[2..-1] ]
+      when '_func' then tree
+      else tree[1]
       end
 
     wrap_reply
