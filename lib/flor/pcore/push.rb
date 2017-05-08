35fd1b3e7b6749deaa37e7b21ded6706be47000b
John Mettraux <jmettraux@gmail.com>
2017-05-08 15:59:51 +0900

    Adapt "push" to potential "_unatt"

--- lib/flor/pcore/push.rb
+++ lib/flor/pcore/push.rb
@@ -1,7 +1,7 @@
 
 class Flor::Pro::Push < Flor::Procedure
 
-  names 'push', 'pushr'
+  names %w[ push pushr ]
 
   def pre_execute
 
@@ -11,12 +11,28 @@ class Flor::Pro::Push < Flor::Procedure
 
   def receive_non_att
 
-    @node['arr'] ||= payload['ret']
+    if ! @node['arr']
+      @node['arr'] = payload['ret']
+    else
+      @node['to_push'] = payload['ret']
+    end
 
     super
   end
 
   def receive_last
+
+    push(@node.has_key?('to_push') ? @node['to_push'] : node_payload_ret)
+
+    payload['ret'] = node_payload_ret \
+      unless tree[0] == 'pushr'
+
+    wrap_reply
+  end
+
+  protected
+
+  def push(val)
 
     arr = @node['arr']
 
@@ -29,17 +45,7 @@ class Flor::Pro::Push < Flor::Procedure
       "cannot push to given target (#{arr.class})", self
     ) unless arr.respond_to?(:push)
 
-    val =
-      unkeyed_children.size > 1 ?
-      payload['ret'] :
-      node_payload_ret
-
     arr.push(val)
-
-    payload['ret'] = node_payload_ret \
-      unless tree[0] == 'pushr'
-
-    wrap_reply
   end
 end
 
