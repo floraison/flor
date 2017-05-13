395ae9a82759f98f9f08fe73cc8387fffab573fe
John Mettraux <jmettraux@gmail.com>
2017-05-13 13:39:47 +0900

    Make "and" and "or" short circuit

--- lib/flor/pcore/logo.rb
+++ lib/flor/pcore/logo.rb
@@ -1,25 +1,47 @@
 
 class Flor::Pro::Logo < Flor::Procedure
-  #
-  # "and" has higher precedence than "or"
 
   names %w[ and or ]
 
-  def pre_execute
+  def execute
 
-    @node['rets'] = []
+    payload['ret'] = @node['heat0'] == 'and'
+
+    super
   end
 
-  def receive_last
+  def receive_att
 
-    payload['ret'] =
-      if @node['heat0'] == 'or'
-        !! @node['rets'].index { |r| Flor.true?(r) }
-      else
-        ! @node['rets'].index { |r| Flor.false?(r) }
-      end
+    c = children[@fcid]; return super if c[0] == '_att' && [1].size == 2
 
-    wrap_reply
+    h0 = @node['heat0']
+
+    ret = Flor.true?(payload['ret'])
+
+    return wrap_reply if ((h0 == 'or' && ret) || (h0 == 'and' && ! ret))
+
+    super
   end
+
+  alias receive_non_att receive_att
+
+#  def pre_execute
+#
+#    @node['rets'] = []
+#  end
+#
+#  def receive_last
+#
+#    payload['ret'] =
+#      if @node['heat0'] == 'or'
+#        !! @node['rets'].index { |r| Flor.true?(r) }
+#      else
+#        ! @node['rets'].index { |r| Flor.false?(r) }
+#      end
+#
+#    wrap_reply
+#  end
+  #
+  # Keep me around for a "aand" and a "oor"... Maybe...
 end
 
