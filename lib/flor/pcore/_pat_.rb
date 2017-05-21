5306275f53a201ab28e77dc6cf70cd45433418eb
John Mettraux <jmettraux@gmail.com>
2017-05-22 06:08:13 +0900

    Complete simple, shallow, "_pat_arr"

--- lib/flor/pcore/_pat_.rb
+++ lib/flor/pcore/_pat_.rb
@@ -12,7 +12,7 @@ class Flor::Pro::PatContainer < Flor::Procedure
 
     return wrap_reply(
       'nid' => nid, 'from' => Flor.child_nid(nid, index, sub)
-    ) if ct.is_a?(String) # short circuit
+    ) if ct.is_a?(String) || ct.is_a?(Array)
 
 #    if ct == :pattern
 ## TODO inject _pat_val
@@ -29,19 +29,26 @@ class Flor::Pro::PatContainer < Flor::Procedure
     node_payload['_pat_val'] || node_payload_ret
   end
 
-  def child_type(cid)
+  def child_type(cid_or_tree)
 
-    ct = tree[1][cid]; ct0 = ct[0]
+    ct = cid_or_tree.is_a?(Array) ? cid_or_tree : tree[1][cid_or_tree]
+    ct0 = ct[0]
 
+    return :att if ct0 == '_att'
     return :pattern if ct0 == '_pat_arr' || ct0 == '_pat_obj'
     return '_' if ct0 == '_'
     return ct0 if ct0.match(/\A[a-z][a-z0-9]*\z/) && ct[1] == []
+
+    m = ct0.match(Flor::SPLAT_REGEX)
+    return [ m[1], m[2] == '_' ? nil : m[2].to_i ] if m
+
     nil # nothing special
   end
 
   def wrap_no_match_reply
 
     payload['_pat_binding'] = nil
+    payload.delete('_pat_val')
 
     wrap_reply
   end
@@ -63,15 +70,27 @@ class Flor::Pro::PatArr < Flor::Pro::PatContainer
     ct = child_type(@fcid)
 #p [ :rna, :fcid, @fcid, :index, @node['index'], :ct, ct ]
 
+    offset = 1
+
     if ct == :pattern
+
 # TODO
+
     elsif ct.is_a?(String)
+
       @node['binding'][ct] = val[@node['index']] if ct != '_'
+
+    elsif ct.is_a?(Array)
+
+      offset = val[@node['index']..-1].size - remaining_index_count
+      @node['binding'][ct[0]] = val[@node['index'], offset]
+
     elsif val[@node['index']] != payload['ret']
+
       return wrap_no_match_reply
     end
 
-    @node['index'] = @node['index'] + 1
+    @node['index'] = @node['index'] + offset
 
     super
   end
@@ -79,11 +98,37 @@ class Flor::Pro::PatArr < Flor::Pro::PatContainer
   def receive_last
 
     payload['_pat_binding'] = @node['binding']
+#    payload.delete('_pat_val')
 
     super
   end
 
   protected
+
+#  def debug(opts={})
+#
+#    nkeys = %w[ nid heat0 ] + (opts[:nkeys] || [])
+#
+#    puts Flor.colours.green + "---procedure-debug- #{object_id}"
+#    caller[0, 3].each { |l| puts "  #{l}" }
+#    print '  '; p(fcid: @fcid, ncid: @ncid)
+#    print '  '; p(@node.select { |k, v| nkeys.include?(k) })
+#    children.each_with_index { |ct, i| puts "  #{i}: #{ct.inspect}" }
+#    puts "---procedure-debug- #{object_id}." + Flor.colours.reset
+#  end
+
+  def remaining_index_count
+
+#debug(nkeys: %w[ val index binding ])
+    children[@ncid..-1]
+      .inject(0) { |count, nact|
+        count +
+        case (ct = child_type(nact))
+        when Array then ct[1] || 1 # FIXME !!!
+        when :att then 0
+        else 1
+        end }
+  end
 end
 
 class Flor::Pro::PatObj < Flor::Pro::PatContainer
--- spec/pcore/_pat_arr_spec.rb
+++ spec/pcore/_pat_arr_spec.rb
@@ -22,7 +22,19 @@ describe 'Flor procedures' do
       [ %q{ _pat_arr \ 7 }, [ 6 ], nil ],
       [ %q{ _pat_arr \ 7 }, [ 7 ], {} ],
       [ %q{ _pat_arr \ a }, [ 5 ], { 'a' => 5 } ],
-      [ %q{ _pat_arr \ _ ; b }, [ 4, 5 ], { 'b' => 5 } ]
+      [ %q{ _pat_arr \ _; b }, [ 4, 5 ], { 'b' => 5 } ],
+
+      [ %q{ _pat_arr \ a; b___; c },
+        [ 4, 5, 6, 7 ],
+        { 'a' => 4, 'b' => [ 5, 6 ], 'c' => 7 } ],
+
+      [ %q{ _pat_arr \ a; b___; c; d },
+        [ 4, 5, 6, 7, 8 ],
+        { 'a' => 4, 'b' => [ 5, 6 ], 'c' => 7, 'd' => 8 } ],
+
+      [ %q{ _pat_arr \ a; b___; c__2; d },
+        [ 1, 2, 3, 4, 5, 6, 7, 8, 9 ],
+        { 'a' => 1, 'b' => [ 2, 3, 4, 5, 6 ], 'c' => [ 7, 8 ], 'd' => 9 } ]
 
     ].each do |code, val, expected|
 
@@ -35,6 +47,7 @@ describe 'Flor procedures' do
 
         expect(r['point']).to eq('terminated')
         expect(r['payload']).to have_key('_pat_binding')
+        expect(r['payload']).not_to have_key('_pat_val')
         expect(r['payload']['_pat_binding']).to eq(expected)
       end
     end
