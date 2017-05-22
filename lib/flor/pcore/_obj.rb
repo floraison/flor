82f365449e957e82d62930643cbab611a6654300
John Mettraux <jmettraux@gmail.com>
2017-05-23 06:49:52 +0900

    Rename "_obj" #stringify to #deref_and_stringify

--- lib/flor/pcore/_obj.rb
+++ lib/flor/pcore/_obj.rb
@@ -49,26 +49,27 @@ class Flor::Pro::Obj < Flor::Procedure
     return wrap_reply('ret' => {}) if children == 0
 
     cn = children
-      .inject([]) { |a, e| a << (a.size.even? ? stringify(e) : e); a }
+      .inject([]) { |a, e|
+        a << (a.size.even? ? deref_and_stringify(e) : e)
+        a }
 
-    @node['tree'] = [ tree[0], cn, tree[2] ] if children != cn
+    @node['tree'] = [ tree[0], cn, *tree[2..-1] ] if children != cn
 
     super
   end
 
   def receive_last
 
-    payload['ret'] =
-      @node['rets']
-        .each_slice(2)
-        .inject({}) { |h, (k, v)| h[k.to_s] = v; h }
+    payload['ret'] = @node['rets']
+      .each_slice(2)
+      .inject({}) { |h, (k, v)| h[k.to_s] = v; h }
 
     wrap_reply
   end
 
   protected
 
-  def stringify(t)
+  def deref_and_stringify(t)
 
     return t unless t[1] == [] && t[0].is_a?(String)
     [ '_sqs', deref(t[0]) || t[0], t[2] ]
