4cb3cfdd56f82d0e96dcd1438fa607f00d9a0b41
John Mettraux <jmettraux@gmail.com>
2017-06-17 06:01:27 +0900

    Added more explanations to quickstart

--- quickstart/quickstart.rb
+++ quickstart/quickstart.rb
@@ -4,28 +4,44 @@
 require 'flor/unit'
 
 FLOR = Flor::Unit.new('flor/etc/conf.json')
-  # configuration is all there
+  # Configuration is all there
+  # It points DB to flor/var/flor.db
+  # Binds this flor unit instances behind the `FLOR` name
 
 FLOR.storage.delete_tables
 FLOR.storage.migrate
+  # Prepare fresh database
+
 FLOR.start
-  # prepare fresh database
+  # Start the flor unit
+  # That starts a thread ticking for messages (launch messages, tasker replies,
+  # etc)
 
 msg =
   FLOR.launch(
-    %{
-      concurrence
-        alice _
-        bob _
+    %q{
+      concurrence     #
+        alice _       # the "workflow definition",
+        bob _         # the 'program' that flor interprets
     },
     domain: 'org.example',
     wait: true)
-  # launch a simple flow fanning work to alice and bob
+      #
+      # Launch a simple flow handing work tasks to alice and bob concurrently
+      # "concurrence" will wait for alice and bob to reply
+      # `wait: true` tells the #launch to wait for the flow to end
+      # The flow will end when its root procedure ("concurrence" here) ends
+      #
+      # "alice" and "bob" are 'taskers', their implementations can be found
+      # under quickstart/flor/lib/taskers/org.example/alice/ and
+      # quickstart/flor/lib/taskers/org.example/bob/ respectively
 
 sleep 0.5
-  # give time to executor to shutdown and stop spitting debug info
+  # Give time to executor to shutdown and stop spitting debug info
 
 pp msg
+  # Pretty print the "terminated" message (could be a "failed" message if
+  # something went wrong)
 
 #FLOR.join
 
