07848f17f53087354c4fce1f1fc318ebdde2efe9
John Mettraux <jmettraux@gmail.com>
2017-12-31 21:23:28 +0900

    Document "reduce"

--- lib/flor/pcore/reduce.rb
+++ lib/flor/pcore/reduce.rb
@@ -1,5 +1,35 @@
 
 class Flor::Pro::Reduce < Flor::Pro::Iterator
+  #
+  # Reduce takes a collection and a function. It reduces the collection
+  # to a single result thanks to the function.
+  #
+  # ```
+  # reduce [ '0', 1, 'b', 3 ]
+  #   def result element
+  #     result + element
+  # # --> "01b3"
+  # ```
+  #
+  # An initial value is accepted (generally after the collection)
+  #
+  # ```
+  # reduce [ 0, 1, 2, 3, 4 ] 10
+  #   def result i \ result + i
+  # # --> 20
+  # ```
+  #
+  # Passing a proc is OK too, but, in the case of a mathematical expression
+  # prefixing it with `v.` prevents premature rewriting...
+  #
+  # ```
+  # reduce [ 0, 1, 2, 3, 4 ] 10 v.+
+  # # --> 20
+  # ```
+  #
+  # ## see also
+  #
+  # Inject.
 
   name 'reduce'
 
