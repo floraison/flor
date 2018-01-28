a838dc00dcf935328f57c26fadb686dd5d474bb6
John Mettraux <jmettraux@gmail.com>
2018-01-28 15:46:09 +0900

    Add "for-each" incoming ret array/object specs

--- spec/pcore/for_each_spec.rb
+++ spec/pcore/for_each_spec.rb
@@ -49,6 +49,41 @@ describe 'Flor procedures' do
       expect(r['payload']['ret']
         ).to eq({ 'a' => 'A', 'b' => 'B', 'c' => 'C' })
     end
+
+    it 'iterates over the incoming ret array' do
+
+      r = @executor.launch(
+        %q{
+          set l []
+          [ 0 1 2 3 4 5 6 7 ]
+          for-each
+            def x
+              pushr l (2 * x) if x % 2 == 0
+        })
+
+      expect(r['point']).to eq('terminated')
+      expect(r['vars']).to eq({ 'l' => [ 0, 4, 8, 12 ] })
+      expect(r['payload']['ret']).to eq((0..7).to_a)
+    end
+
+    it 'iterates over the incoming ret object' do
+
+      r = @executor.launch(
+        %q{
+          set l []
+          { a: 'A', b: 'B', c: 'C' }
+          for-each
+            def k v i
+              pushr l (+ k v i)
+        })
+
+      expect(r['point']
+        ).to eq('terminated')
+      expect(r['vars']
+        ).to eq({ 'l' => %w[ aA0 bB1 cC2 ] })
+      expect(r['payload']['ret']
+        ).to eq({ 'a' => 'A', 'b' => 'B', 'c' => 'C' })
+    end
   end
 end
 
