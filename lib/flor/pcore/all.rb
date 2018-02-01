d2370a1ec56cb8bdd5deb63ce961d2820e389d05
John Mettraux <jmettraux@gmail.com>
2018-02-01 12:54:14 +0900

    Document "all?" without a function

--- lib/flor/pcore/all.rb
+++ lib/flor/pcore/all.rb
@@ -26,6 +26,17 @@ class Flor::Pro::All < Flor::Pro::Iterator
   #     # yields true
   # ```
   #
+  # ### without a function
+  #
+  # ```
+  # all? []                            # yields true
+  # all? [ 1 2 3 ]                     # yields true
+  # all? [ 1 false 3 ]                 # yields false
+  # all? {}                            # yields true
+  # all? { a: 'A', b: 'B', c: 'C' }    # yields true
+  # all? { a: 'A', f: false, c: 'C' }  # yields false
+  # ```
+  #
   # ## see also
   #
   # Any?
