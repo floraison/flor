9da958bc88be59675642ff9b590d881cfcad1679
John Mettraux <jmettraux@gmail.com>
2018-02-05 07:06:41 +0900

    Reorganize the "filter" spec

--- spec/pcore/filter_spec.rb
+++ spec/pcore/filter_spec.rb
@@ -17,7 +17,7 @@ describe 'Flor procedures' do
 
   describe 'filter' do
 
-    it 'filters elements' do
+    it 'filters array elements' do
 
       r = @executor.launch(
         %q{
@@ -30,7 +30,23 @@ describe 'Flor procedures' do
       expect(r['payload']['ret']).to eq([ 1, 3, 5 ])
     end
 
-    it 'filters f.ret by default' do
+    it 'filters object entries' do
+
+      r = @executor.launch(
+        %q{
+          filter { a: 'A', b: 'B', c: 'C', d: 'D' }
+            def k v i
+              #or (k == 'a') (v == 'C') (i == 3)
+              k == 'a' or v == 'C' or i == 3
+        })
+
+      expect(r['point']
+        ).to eq('terminated')
+      expect(r['payload']['ret']
+        ).to eq({ 'a' => 'A', 'c' => 'C', 'd' => 'D' })
+    end
+
+    it 'filters the incoming ret by default' do
 
       r = @executor.launch(
         %q{
@@ -42,30 +58,11 @@ describe 'Flor procedures' do
       expect(r['point']).to eq('terminated')
       expect(r['payload']['ret']).to eq([ 1, 3, 5 ])
     end
-
-    context 'with objects' do
-
-      it 'returns an object with filtered entries' do
-
-        r = @executor.launch(
-          %q{
-            filter { a: 'A', b: 'B', c: 'C', d: 'D' }
-              def k v i
-                #or (k == 'a') (v == 'C') (i == 3)
-                k == 'a' or v == 'C' or i == 3
-          })
-
-        expect(r['point']
-          ).to eq('terminated')
-        expect(r['payload']['ret']
-          ).to eq({ 'a' => 'A', 'c' => 'C', 'd' => 'D' })
-      end
-    end
   end
 
   describe 'filter-out' do
 
-    it 'filters out elements' do
+    it 'filters out array elements' do
 
       r = @executor.launch(
         %q{
@@ -78,19 +75,16 @@ describe 'Flor procedures' do
       expect(r['payload']['ret']).to eq([ 1, 3, 5 ])
     end
 
-    context 'with objects' do
+    it 'filters out object entries' do
 
-      it 'returns an object with filtered out entries' do
+      r = @executor.launch(
+        %q{
+          filter-out { a: 'A', b: 'B', c: 'C', d: 'D' }
+            def k v i \ k == 'a' or k == 'b'
+        })
 
-        r = @executor.launch(
-          %q{
-            filter-out { a: 'A', b: 'B', c: 'C', d: 'D' }
-              def k v i \ k == 'a' or k == 'b'
-          })
-
-        expect(r['point']).to eq('terminated')
-        expect(r['payload']['ret']).to eq({ 'c' => 'C', 'd' => 'D' })
-      end
+      expect(r['point']).to eq('terminated')
+      expect(r['payload']['ret']).to eq({ 'c' => 'C', 'd' => 'D' })
     end
   end
 end
