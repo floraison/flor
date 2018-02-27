ce30dbdfa8a3910b78dbe3113613a0fbb3f8cf83
John Mettraux <jmettraux@gmail.com>
2018-02-28 06:42:31 +0900

    Document "trap" heap: and heat:

--- spec/punit/trap_spec.rb
+++ spec/punit/trap_spec.rb
@@ -361,6 +361,33 @@ describe 'Flor punit' do
         }.collect(&:strip).join("\n"))
       end
 
+      it 'traps a procedure and a point'
+#
+#        r = @unit.launch(
+#          %q{
+#            trap heap: 'sequence', point: 'execute'
+#              def msg
+#                trace "$(msg.point)-$(msg.tree.0)-$(msg.nid)<-$(msg.from)"
+#            sequence
+#              noret _
+#          },
+#          wait: true)
+#
+#        expect(r['point']).to eq('terminated')
+#
+#        wait_until { @unit.traces.count > 2 }
+#
+#        expect(
+#          @unit.traces
+#            .each_with_index
+#            .collect { |t, i| "#{i}:#{t.text}" }.join("\n")
+#        ).to eq(%w{
+#          0:execute-sequence-0_1<-0
+#          1:receive--0_1<-0_1_0
+#          2:receive--0<-0_1
+#        }.collect(&:strip).join("\n"))
+#      end
+
       it 'traps multiple given procedures' do
 
         r = @unit.launch(
@@ -429,7 +456,7 @@ describe 'Flor punit' do
         }.collect(&:strip).join("\n"))
       end
 
-      it 'traps given procedures' do
+      it 'traps given functions' do
 
         r = @unit.launch(
           %q{
