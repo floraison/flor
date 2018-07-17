ec5538cd4477cb01771f79d11c18467129116a10
John Mettraux <jmettraux@gmail.com>
2018-07-17 22:12:46 +0900

    Use `class << self` if lib/flor/id.rb

--- lib/flor/id.rb
+++ lib/flor/id.rb
@@ -1,95 +1,98 @@
 
 module Flor
 
-  #
-  # ids
-  #
-  # functions about exids, nids, sub_nids, ...
+  class << self
 
-  def self.split_fei(fei)
+    #
+    # ids
+    #
+    # functions about exids, nids, sub_nids, ...
 
-    if m = fei.match(/\A([^-]+-[^-]+-\d+\.\d+\.[^-]+)-(.*)\z/)
-      [ m[1], m[2] ]
-    else
-      [ nil ]
+    def split_fei(fei)
+
+      if m = fei.match(/\A([^-]+-[^-]+-\d+\.\d+\.[^-]+)-(.*)\z/)
+        [ m[1], m[2] ]
+      else
+        [ nil ]
+      end
     end
-  end
 
-  def self.exid(fei)
+    def exid(fei)
 
-    split_fei(fei).first
-  end
-
-  def self.split_nid(nid)
-
-    nid.split('-')
-  end
-
-  def self.child_id(nid)
-
-    nid ? nid.split('_').last.split('-').first.to_i : nil
-  end
-
-  def self.next_child_id(nid)
-
-    child_id(nid) + 1
-  end
-
-  def self.sub_nid(nid, subid=nil)
-
-    if subid
-      "#{nid.split('-').first}-#{subid}"
-    else
-      ss = nid.split('-')
-      ss.length > 1 ? ss.last.to_i : 0
+      split_fei(fei).first
     end
-  end
 
-  # Remove the sub_nid if any.
-  #
-  def self.master_nid(nid)
+    def split_nid(nid)
 
-    nid.split('-').first
-  end
-
-  def self.child_nid(nid, i, sub=nil)
-
-    nid, subnid = nid.split('-')
-    subnid = sub if sub && sub > 0
-
-    "#{nid}_#{i}#{subnid ? "-#{subnid}" : ''}"
-  end
-
-  def self.parent_id(nid)
-
-    if i = nid.rindex('_')
-      nid[0, i]
-    else
-      nil
+      nid.split('-')
     end
-  end
 
-  def self.parent_nid(nid, remove_subnid=false)
+    def child_id(nid)
 
-    _, sub = nid.split('-')
-    i = nid.rindex('_')
+      nid ? nid.split('_').last.split('-').first.to_i : nil
+    end
 
-    return nil unless i
-    "#{nid[0, i]}#{remove_subnid || sub.nil? ? nil : "-#{sub}"}"
-  end
+    def next_child_id(nid)
 
-  def self.is_nid?(s)
+      child_id(nid) + 1
+    end
 
-    !! (s.is_a?(String) && s.match(/\A[0-9]+(?:_[0-9]+)*(?:-[0-9]+)?\z/))
-  end
+    def sub_nid(nid, subid=nil)
 
-  # Returns [ exid, nid ]
-  #
-  def self.extract_exid_and_nid(s)
+      if subid
+        "#{nid.split('-').first}-#{subid}"
+      else
+        ss = nid.split('-')
+        ss.length > 1 ? ss.last.to_i : 0
+      end
+    end
 
-    m = s.match(/(\d{8}\.\d{4}\.[a-z]+)-(\d+(?:_\d+)*)(-\d+)?/)
+    # Remove the sub_nid if any.
+    #
+    def master_nid(nid)
 
-    m ? [ m[1], [ m[2], m[3] ].compact.join ] : nil
+      nid.split('-').first
+    end
+
+    def child_nid(nid, i, sub=nil)
+
+      nid, subnid = nid.split('-')
+      subnid = sub if sub && sub > 0
+
+      "#{nid}_#{i}#{subnid ? "-#{subnid}" : ''}"
+    end
+
+    def parent_id(nid)
+
+      if i = nid.rindex('_')
+        nid[0, i]
+      else
+        nil
+      end
+    end
+
+    def parent_nid(nid, remove_subnid=false)
+
+      _, sub = nid.split('-')
+      i = nid.rindex('_')
+
+      return nil unless i
+      "#{nid[0, i]}#{remove_subnid || sub.nil? ? nil : "-#{sub}"}"
+    end
+
+    def is_nid?(s)
+
+      !! (s.is_a?(String) && s.match(/\A[0-9]+(?:_[0-9]+)*(?:-[0-9]+)?\z/))
+    end
+
+    # Returns [ exid, nid ]
+    #
+    def extract_exid_and_nid(s)
+
+      m = s.match(/(\d{8}\.\d{4}\.[a-z]+)-(\d+(?:_\d+)*)(-\d+)?/)
+
+      m ? [ m[1], [ m[2], m[3] ].compact.join ] : nil
+    end
   end
 end
 
