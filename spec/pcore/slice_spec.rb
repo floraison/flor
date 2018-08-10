9a8a8f3a6f25b9f098b1ee9f873836e8bc1f9f5f
John Mettraux <jmettraux@gmail.com>
2018-08-10 17:55:36 +0200

    Spec "slice" against arrays and strings

--- spec/pcore/slice_spec.rb
+++ spec/pcore/slice_spec.rb
@@ -17,21 +17,17 @@ describe 'Flor procedures' do
 
   describe 'slice' do
 
-    context 'array' do
+    context 'with array' do
 
       {
 
-        [ 1, -1 ] => %w[ b c d e f g ],
+        '1, -1' => %w[ b c d e f g ],
+        'from: 1, to: -1' => %w[ b c d e f g ],
+        '1, count: 2' => %w[ b c ],
 
       }.each do |args, ret|
 
-        it "#{args.inspect} to #{ret}" do
-
-          args =
-            case args
-            when Array then args.collect(&:to_s).join(', ')
-            else args.collect { |k, v| "#{k}: #{v}" }.join(', ')
-            end
+        it "slices #{args}" do
 
           r =
             @executor.launch(%{
@@ -44,22 +40,56 @@ describe 'Flor procedures' do
           expect(r['payload']['ret']).to eq(ret)
         end
       end
+
+      it 'slices' #do
+#
+#        r =
+#          @executor.launch(%{
+#            slice a 1 count: 2
+#          },
+#          vars: { 'a' => %w[ a b c d e f g ] })
+#
+#        expect(r['point']).to eq('terminated')
+#        expect(r['payload']['ret']).to eq(%w[ b c ])
+#      end
     end
 
-    context 'string' do
+    context 'with string' do
 
-      it 'works'
+      {
+
+        '1, -1' => 'bcdefg',
+        'from: 1, to: -1' => 'bcdefg',
+        '1, count: 2' => 'bc',
+
+      }.each do |args, ret|
+
+        it "slices #{args}" do
+
+          r =
+            @executor.launch(%{
+              s
+              slice #{args}
+            },
+            vars: { 's' => 'abcdefg' })
+
+          expect(r['point']).to eq('terminated')
+          expect(r['payload']['ret']).to eq(ret)
+        end
+      end
+
+      it 'slices'
     end
   end
 
   describe 'index' do
 
-    context 'array' do
+    context 'with array' do
 
       it 'works'
     end
 
-    context 'string' do
+    context 'with string' do
 
       it 'works'
     end
