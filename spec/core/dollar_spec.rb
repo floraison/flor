792f0a3486b37b65d29ce00542ba1b3e024dcfbc
John Mettraux <jmettraux@gmail.com>
2018-10-04 14:42:09 +0900

    Adapt dollar spec to revised "capitalize"

--- spec/core/dollar_spec.rb
+++ spec/core/dollar_spec.rb
@@ -104,7 +104,9 @@ describe 'Flor core' do
       '$( bs | downcase _ )' => 'black sheep',
       '$( bs | upcase _ )' => 'black sheep'.upcase,
       '$( ba | reverse _ )' => 'black adder'.reverse,
-      '$( ba | capitalize _ )' => 'Black adder',
+      '$( ba | capitalize _ )' => 'Black Adder',
+      '$( ba | downcase _ )' => 'black adder',
+      '$( ba | downcase cap: true )' => 'Black adder',
 
       # slice
       '$( quick | slice 1, -1 )' => 'ump',
