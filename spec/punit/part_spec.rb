8d2e64fc4e4060d4a50475e051dfa55af3ca0713
John Mettraux <jmettraux@gmail.com>
2018-11-02 08:54:40 +0900

    Scaffold "flunk" spec (have to find better name)

--- spec/punit/part_spec.rb
+++ spec/punit/part_spec.rb
@@ -13,7 +13,7 @@ describe 'Flor punit' do
   before :each do
 
     @unit = Flor::Unit.new('envs/test/etc/conf.json')
-    @unit.conf['unit'] = 'forktest'
+    @unit.conf['unit'] = 'parttest'
     #@unit.hooker.add('journal', Flor::Journal)
     @unit.storage.delete_tables
     @unit.storage.migrate
@@ -25,9 +25,20 @@ describe 'Flor punit' do
     @unit.shutdown
   end
 
+  #                  +-------------------+--------------------+
+  #                  | replies to parent | cancellable        |
+  # +-------+--------+-------------------+--------------------+
+  # | fork  | part   | immediately       | no (not reachable) |
+  # |       | flunk  | never             | no (not reachable) |
+  # | flank | flank  | immediately       | yes                |
+  # | lose  | norep  | never             | yes                |
+  # +-------+--------+-------------------+--------------------+
+  #
+  # reply/r: false, cancellable/c: false
+
   describe 'part' do
 
-    it 'parts' do
+    it 'replies immediately to its parent' do
 
       r = @unit.launch(
         %q{
@@ -51,6 +62,36 @@ describe 'Flor punit' do
         'b c a'
       )
     end
+
+    it 'may be cancelled explicitely'
+    it 'does not get cancelled when its parent gets cancelled'
+  end
+
+  describe 'flunk' do
+
+    it 'does not reply to its parent'# do
+#
+#      r = @unit.launch(
+#        %q{
+#          concurrence count: 1
+#            flunk
+#              _skip 4
+#              trace 'a'
+#            trace 'b'
+#          trace 'c'
+#        },
+#        wait: true)
+#
+#      expect(r['point']).to eq('terminated')
+#      expect(r['payload']['ret']).to eq(nil)
+#      expect(r['payload']['parted']).to eq('0_0_0_1')
+#
+#      expect(
+#        @unit.traces.collect(&:text).join(' ')
+#      ).to eq(
+#        'b c a'
+#      )
+#    end
 
     it 'may be cancelled explicitely'
     it 'does not get cancelled when its parent gets cancelled'
