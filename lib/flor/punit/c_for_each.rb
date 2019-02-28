b39c4de72620eced59dd4b713f050bc28cc61795
John Mettraux <jmettraux@gmail.com>
2019-02-28 09:19:58 +0900

    Implement "c-for-each"

---
+++ lib/flor/punit/c_for_each.rb
@@ -0,0 +1,87 @@
+
+class Flor::Pro::CforEach < Flor::Procedure
+
+  name 'c-for-each'
+
+  def pre_execute
+
+    @node['args'] = []
+    @node['col'] = nil
+
+    unatt_unkeyed_children
+  end
+
+  def receive_non_att
+
+    if @node['col']
+      receive_ret
+    else
+      @node['args'] << payload['ret']
+      super
+    end
+  end
+
+  def receive_last
+
+    if @node['col']
+      super
+    else
+      receive_last_argument
+    end
+  end
+
+  protected
+
+  def receive_last_argument
+
+    col = nil
+    fun = nil
+    @node['args'].each do |a|
+      if Flor.is_func_tree?(a)
+        fun = a
+      elsif Flor.is_collection?(a)
+        col = a
+      end
+    end
+    col ||= node_payload_ret
+
+    fail Flor::FlorError.new("collection not given to #{heap.inspect}", self) \
+      unless Flor.is_collection?(col)
+    return wrap('ret' => col) \
+      unless Flor.is_func_tree?(fun)
+
+    @node['col'] = col
+    @node['cnt'] = col.size
+
+    col
+      .collect
+      .with_index { |e, i|
+        apply(fun, determine_iteration_args(col, i), tree[2]) }
+      .flatten(1)
+  end
+
+  def determine_iteration_args(col, idx)
+
+    args =
+      if col.is_a?(Array)
+        [ [ 'elt', col[idx] ] ]
+      else
+        e = col.to_a[idx]
+        [ [ 'key', e[0] ], [ 'val', e[1] ] ]
+      end
+    args << [ 'idx', idx ]
+    args << [ 'len', col.length ]
+
+    args
+  end
+
+  def receive_ret
+
+    @node['cnt'] = @node['cnt'] - 1
+
+    return [] if @node['cnt'] > 0 # still waiting for answers
+
+    wrap('ret' => @node['col']) # over
+  end
+end
+
---
+++ spec/punit/c_for_each_spec.rb
@@ -0,0 +1,246 @@
+
+#
+# specifying flor
+#
+# Thu Feb 28 08:55:58 JST 2019
+#
+
+require 'spec_helper'
+
+
+describe 'Flor punit' do
+
+  before :each do
+
+    @unit = Flor::Unit.new('envs/test/etc/conf.json')
+    @unit.conf['unit'] = 'pu_c_for_each'
+    @unit.hooker.add('journal', Flor::Journal)
+    @unit.storage.delete_tables
+    @unit.storage.migrate
+    @unit.start
+  end
+
+  after :each do
+
+    @unit.shutdown
+  end
+
+  describe 'c-for-each' do
+
+    it 'executes atts in sequence then children in concurrence' do
+
+      r = @unit.launch(
+        %q{
+          set l []
+          c-for-each [ 1 2 3 ]
+            def x \ push l (* x 2)
+        },
+        wait: true)
+
+      expect(r).to have_terminated_as_point
+      expect(r['payload']['ret']).to eq([ 1, 2, 3 ])
+      expect(r['vars']['l']).to eq([ 2, 4, 6 ])
+    end
+
+#    it 'shows the index via the "idx" var' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap [ 10 11 12 ]
+#            def x \ [ idx x ]
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#      expect(r['payload']['ret']).to eq([ [ 0, 10 ], [ 1, 11 ], [ 2, 12 ] ])
+#    end
+#
+#    it 'passes the index as second function arg if possible' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap [ 10 11 12 ]
+#            def x i \ [ i x ]
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#      expect(r['payload']['ret']).to eq([ [ 0, 10 ], [ 1, 11 ], [ 2, 12 ] ])
+#    end
+#
+#    it 'passes the length as third function arg if possible' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap [ 10 11 12 ]
+#            def x i l \ [ i l x ]
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq([
+#        [ 0, 3, 10 ], [ 1, 3, 11 ], [ 2, 3, 12 ]
+#      ])
+#    end
+#
+#    it 'preserves the children order' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap [ 1 2 3 ]
+#            def x
+#              sleep 0.4 if (x % 2) == 0
+#              x
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#      expect(r['payload']['ret']).to eq([ 1, 2, 3 ])
+#    end
+#
+#    it 'iterates over objects' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap { name: 'joe' age: 45 drink: 'coffee' }
+#            def key, val, idx, len
+#              "$(key):$(val):$(idx):$(len)"
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq([
+#        'name:joe:0:3', 'age:45:1:3', 'drink:coffee:2:3'
+#      ])
+#    end
+#
+#    it 'iterates over objects (and sets vars)' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap { name: 'joe' age: 45 drink: 'coffee' }
+#            def k, v, i, l
+#              "$(k):$(v):$(i):$(l)"
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq([
+#        'name:joe:0:3',
+#        'age:45:1:3',
+#        'drink:coffee:2:3',
+#      ])
+#    end
+#
+#    it 'iterates over the incoming f.ret (array)' do
+#
+#      r = @unit.launch(
+#        %q{
+#          [ 0 1 2 3 4 5 6 ]
+#          cmap
+#            def e, i, l \ "$(e):$(i):$(l)"
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq(%w[
+#        0:0:7 1:1:7 2:2:7 3:3:7 4:4:7 5:5:7 6:6:7
+#      ])
+#    end
+#
+#    it 'iterates over the incoming f.ret (object)' do
+#
+#      r = @unit.launch(
+#        %q{
+#          { name: 'joe' age: 45 drink: 'coffee' }
+#          cmap
+#            def k, v, i, l \ "$(k):$(v):$(i):$(l)"
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq([
+#        'name:joe:0:3', 'age:45:1:3', 'drink:coffee:2:3'
+#      ])
+#    end
+#
+#    it 'iterates over the child collection (object)' do
+#
+#      r = @unit.launch(
+#        %q{
+#          cmap
+#            { name: 'Jocko' age: 45 drink: 'tea' }
+#            def k, v, i, l \ "$(k):$(v):$(i):$(l)"
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#
+#      expect(
+#        r['payload']['ret']
+#      ).to eq([
+#        'name:Jocko:0:3', 'age:45:1:3', 'drink:tea:2:3'
+#      ])
+#    end
+#
+#    it 'fails if it is not given a collection' do
+#
+#      r = @unit.launch(
+#        %q{
+#          1
+#          cmap
+#            def k v i \ [ i k v ]
+#        },
+#        wait: true)
+#
+#      expect(r['point']).to eq('failed')
+#      expect(r['error']['msg']).to eq('collection not given to "cmap"')
+#    end
+#
+#    it 'returns the collection if it is not given a function' do
+#
+#      r = @unit.launch(
+#        %q{
+#          [ 0 1 2 3 ]
+#          cmap _
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#      expect(r['payload']['ret']).to eq([ 0, 1, 2, 3 ])
+#    end
+#
+#    it 'does not mind attributes and non-coll, non-fun children' do
+#
+#      r = @unit.launch(
+#        %q{
+#          [ 0 1 2 3 ]
+#          cmap [ 0 1 2 ]
+#            "and so it goes"
+#            def elt \ + elt 3
+#            true
+#        },
+#        wait: true)
+#
+#      expect(r).to have_terminated_as_point
+#      expect(r['payload']['ret']).to eq([ 3, 4, 5 ])
+#    end
+  end
+end
+
+
