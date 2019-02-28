a4c13812b756ee1af7f0d5b55c8dcfc673a069f9
John Mettraux <jmettraux@gmail.com>
2019-02-28 22:55:46 +0900

    Introduce Flor::Pro::ConcurrentIterator

--- lib/flor/punit/cmap.rb
+++ lib/flor/punit/cmap.rb
@@ -1,5 +1,8 @@
 
-class Flor::Pro::Cmap < Flor::Procedure
+require 'flor/punit/citerator'
+
+
+class Flor::Pro::Cmap < Flor::Pro::ConcurrentIterator
   #
   # Concurrent version of "map". Spins a concurrent child for each
   # element of the incoming/argument collection.
@@ -29,75 +32,12 @@ class Flor::Pro::Cmap < Flor::Procedure
 
   def pre_execute
 
-    @node['args'] = []
-    @node['result'] = nil
+    @node['result'] = []
 
-    unatt_unkeyed_children
-  end
-
-  def receive_non_att
-
-    if @node['result']
-      receive_ret
-    else
-      @node['args'] << payload['ret']
-      super
-    end
-  end
-
-  def receive_last
-
-    if @node['result']
-      super
-    else
-      receive_last_argument
-    end
+    super
   end
 
   protected
-
-  def receive_last_argument
-
-    col = nil
-    fun = nil
-    @node['args'].each do |a|
-      if Flor.is_func_tree?(a)
-        fun = a
-      elsif Flor.is_collection?(a)
-        col = a
-      end
-    end
-    col ||= node_payload_ret
-
-    fail Flor::FlorError.new("collection not given to #{heap.inspect}", self) \
-      unless Flor.is_collection?(col)
-    return wrap('ret' => col) \
-      unless Flor.is_func_tree?(fun)
-
-    @node['cnt'] = col.size
-    @node['result'] = []
-
-    col
-      .collect
-      .with_index { |e, i|
-        apply(fun, determine_iteration_args(col, i), tree[2]) }
-      .flatten(1)
-  end
-
-  def determine_iteration_args(col, idx)
-
-    args =
-      if col.is_a?(Array)
-        [ [ 'elt', col[idx] ] ]
-      else
-        e = col.to_a[idx]
-        [ [ 'key', e[0] ], [ 'val', e[1] ] ]
-      end
-    args << [ 'idx', idx ]
-    args << [ 'len', col.length ]
-
-    args
-  end
 
   def receive_ret
 
