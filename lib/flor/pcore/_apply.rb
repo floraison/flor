6696f75c61023e353cfe1654d851b1fbbff22d74
John Mettraux <jmettraux@gmail.com>
2019-03-13 08:00:16 +0900

    Reformat _apply arg to sig matching

--- lib/flor/pcore/_apply.rb
+++ lib/flor/pcore/_apply.rb
@@ -27,8 +27,10 @@ class Flor::Pro::UnderscoreApply < Flor::Procedure
     atts = tr[1].inject([]) { |a, t| a << t[1] if t[0] == '_att'; a }
     args = args.dup
 #puts "\n---"
-#puts "== atts (sig):"; atts.each { |a| pp a }    # signature
-#puts "== args (given):"; args.each { |a| pp a }  # passed arguments
+#puts "== atts (sig):"
+#atts.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
+#puts "== args:"
+#args.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
 #puts
 
     # first, make 2 passes on atts
@@ -39,33 +41,33 @@ class Flor::Pro::UnderscoreApply < Flor::Procedure
 
     seen = []
 
-    atts
-      .each { |(att_key, _), _|
-        arg_i = args.index { |arg_key, arg_val| arg_key == att_key }
-        next unless arg_i
-        arg_key, arg_val = args.delete_at(arg_i)
-        seen << arg_key
-        vars[att_key] = arg_val }
+    atts.each do |(att_key, _), _|
+      arg_i = args.index { |arg_key, arg_val| arg_key == att_key }
+      next unless arg_i
+      arg_key, arg_val = args.delete_at(arg_i)
+      seen << arg_key
+      vars[att_key] = arg_val
+    end
 #puts "== 0 vars:"; pp vars
-    atts
-      .each_with_index { |((att_key, _), att_val), att_i|
-        next if vars.has_key?(att_key)
-        seen << att_key
-        if att_i < args.length
-          arg_key, arg_val = args[att_i]
-          seen << arg_key
-          vars[att_key] = arg_val
-        elsif att_val
-          l = tree[2]
-          retree ||= Flor.dup(tr)
-          retree[1][att_i] = [ 'set', [ [ att_key, [], l ], att_val ], l ]
-        else
-          vars[att_key] = nil
-        end }
+    atts.each_with_index do |((att_key, _), att_val), att_i|
+      next if vars.has_key?(att_key)
+      seen << att_key
+      if att_i < args.length
+        arg_key, arg_val = args[att_i]
+        seen << arg_key
+        vars[att_key] = arg_val
+      elsif att_val
+        l = tree[2]
+        retree ||= Flor.dup(tr)
+        retree[1][att_i] = [ 'set', [ [ att_key, [], l ], att_val ], l ]
+      else
+        vars[att_key] = nil
+      end
+    end
 #puts "== seen:"; p seen
-    args
-      .each { |arg_key, arg_val|
-        vars[arg_key] = arg_val unless seen.include?(arg_key) }
+    args.each do |arg_key, arg_val|
+      vars[arg_key] = arg_val unless seen.include?(arg_key)
+    end
 
 #puts "\n== 1 vars: "; pp vars
 #print "vars: "; pp vars.collect { |k, v| [ k, JSON.dump(v)[0, 20] + "..." ] }
