cce501e6b1038bc2d5ec8a2dd4cb20df61fff7f2
John Mettraux <jmettraux@gmail.com>
2019-03-14 08:33:43 +0900

    Spin off #map_arguments_to_parameters in "_apply"

--- lib/flor/pcore/_apply.rb
+++ lib/flor/pcore/_apply.rb
@@ -16,65 +16,7 @@ class Flor::Pro::UnderscoreApply < Flor::Procedure
       @node['in_on_error'] = from
     end
 
-    vars = @node['vars'] = {}
-    args = message['arguments']
-
-    vars['arguments'] = args
-
-    tr = tree
-    retree = nil
-
-    atts = tr[1].inject([]) { |a, t| a << t[1] if t[0] == '_att'; a }
-    args = args.dup
-#puts "\n---"
-#puts "== atts (sig):"
-#atts.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
-#puts "== args:"
-#args.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
-#puts
-
-    # first, make 2 passes on atts
-    # 1). grab named args
-    # 2). grab remaining args
-    # then make 1 pass on still remaining args
-    # 1). set vars if not yet set
-
-    seen = []
-
-    atts.each do |(att_key, _), _|
-      arg_i = args.index { |arg_key, arg_val| arg_key == att_key }
-      next unless arg_i
-      arg_key, arg_val = args.delete_at(arg_i)
-      seen << arg_key
-      vars[att_key] = arg_val
-    end
-#puts "== 0 vars:"; pp vars
-    atts.each_with_index do |((att_key, _), att_val), att_i|
-      next if vars.has_key?(att_key)
-      seen << att_key
-      if att_i < args.length
-        arg_key, arg_val = args[att_i]
-        seen << arg_key
-        vars[att_key] = arg_val
-      elsif att_val
-        l = tree[2]
-        retree ||= Flor.dup(tr)
-        retree[1][att_i] = [ 'set', [ [ att_key, [], l ], att_val ], l ]
-      else
-        vars[att_key] = nil
-      end
-    end
-#puts "== seen:"; p seen
-    args.each do |arg_key, arg_val|
-      vars[arg_key] = arg_val unless seen.include?(arg_key)
-    end
-
-#puts "\n== 1 vars: "; pp vars
-#print "vars: "; pp vars.collect { |k, v| [ k, JSON.dump(v)[0, 20] + "..." ] }
-#print "vars: "; vars.each { |k, v| print "#{k.inspect} --> "; pp v }
-
-    @node['tree'] = retree if retree
-#puts "== retree:"; pp retree; puts
+    map_arguments_to_parameters
 
     super
   end
@@ -105,6 +47,79 @@ class Flor::Pro::UnderscoreApply < Flor::Procedure
     ms[0]['from_on_error'] = ioe if ioe
 
     ms
+  end
+
+  protected
+
+  def map_arguments_to_parameters
+
+    @node['vars'] = {}
+
+    args = message['arguments']
+
+    @node['vars']['arguments'] = args
+
+    tr = tree
+    retree = nil
+
+    params = tr[1].inject([]) { |a, t| a << t[1] if t[0] == '_att'; a }
+    args = args.dup
+#puts "\n---"
+#puts "== params:"
+#params.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
+#puts "== args:"
+#args.each_with_index { |a, i| printf "%2d: %s\n", i, a.inspect }
+#puts
+
+    # first, make 2 passes on atts
+    # 1). grab named args
+    # 2). grab remaining args
+    # then make 1 pass on still remaining args
+    # 1). set vars if not yet set
+
+    seen = []
+
+    params.each do |(param_key, _), _|
+      arg_i = args.index { |arg_key, arg_val| arg_key == param_key }
+      next unless arg_i
+      arg_key, arg_val = args.delete_at(arg_i)
+      seen << arg_key
+      set_param(param_key, arg_val)
+    end
+#puts "== 0 vars:"; pp @node['vars']
+    params.each_with_index do |((param_key, _), param_val), param_i|
+      next if @node['vars'].has_key?(param_key)
+      seen << param_key
+      if param_i < args.length
+        arg_key, arg_val = args[param_i]
+        seen << arg_key
+        set_param(param_key, arg_val)
+      elsif param_val
+        l = tree[2]
+        retree ||= Flor.dup(tr)
+        retree[1][param_i] = [ 'set', [ [ param_key, [], l ], param_val ], l ]
+      else
+        set_param(param_key, nil)
+      end
+    end
+#puts "== seen:"; p seen
+    args.each do |arg_key, arg_val|
+      set_param(arg_key, arg_val) unless seen.include?(arg_key)
+    end
+
+#puts "\n== 1 vars: "; pp @node['vars']
+#print "vars: "
+#pp @node['vars'].collect { |k, v| [ k, JSON.dump(v)[0, 20] + "..." ] }
+#print "vars: "
+#@node['vars'].each { |k, v| print "#{k.inspect} --> "; pp v }
+
+#puts "== retree:"; pp retree; puts
+    @node['tree'] = retree if retree
+  end
+
+  def set_param(key, val)
+
+    @node['vars'][key] = val
   end
 end
 
