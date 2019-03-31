e3ff5bce1e8e5b8487ff2ede16f717d55befe3f2
John Mettraux <jmettraux@gmail.com>
2019-03-31 18:10:45 +0900

    Move flow out of quickstart1/quickstart.rb
    [ci skip]
    
    To show a flor that is in expanded (not compact as in quickstart0/)

--- doc/quickstart1/quickstart.rb
+++ doc/quickstart1/quickstart.rb
@@ -17,27 +17,9 @@ FLOR.start
   # That starts a thread ticking for messages (launch messages, tasker replies,
   # etc)
 
-msg =
-  FLOR.launch(
-    %q{
-      concurrence     #
-        alice _       # the "workflow definition",
-        bob _         # the 'program' that flor interprets
-    },
-    domain: 'org.example',
-    wait: true)
-      #
-      # Launch a simple flow handing work tasks to alice and bob concurrently
-      # "concurrence" will wait for alice and bob to reply
-      # `wait: true` tells the #launch to wait for the flow to end
-      # The flow will end when its root procedure ("concurrence" here) ends
-      #
-      # "alice" and "bob" are 'taskers', their implementations can be found
-      # under quickstart/flor/lib/taskers/org.example/alice/ and
-      # quickstart/flor/lib/taskers/org.example/bob/ respectively
-      #
-      # There is more information about domain: and wait:
-      # in ../doc/launching.md
+msg = FLOR.launch('org.example.flow0', wait: true)
+  # Launches the flow at flor/lib/flows/org.example/flow0.flor
+  # There is more information about domain: and wait: at ../doc/launching.md
 
 sleep 0.5
   # Give time to executor to shutdown and stop spitting debug info
