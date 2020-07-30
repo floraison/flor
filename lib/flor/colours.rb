7b9fc9065827f96625037af31be11395222c6c49
John Mettraux <jmettraux@gmail.com>
2020-07-30 11:25:30 +0900

    Fix colours issue on BSD st

--- lib/flor/colours.rb
+++ lib/flor/colours.rb
@@ -4,7 +4,7 @@ module Flor
   COLS = Hash[*%w[
 
     reset 0;0
-    bright 1 dim 2 underlined 4 blink 5 reverse 7 hidden 8 default 39
+    bright 1 dim 2 underlined 4 blink 5 reverse 7 hidden 8 strike 9 default 39
     black 30 red 31 green 32 yellow 33 blue 34 magenta 35 cyan 36
     light_gray 37 dark_gray 90 light_red 91 light_green 92
     light_yellow 93 light_blue 94 light_magenta 95 light_cyan 96 white 97
@@ -29,7 +29,7 @@ module Flor
       if v.match(/\A\d/)
         class_eval(%{
           def #{k}(s=nil)
-           s ? "[#{v}m" + s + "[0;9m" : "[#{v}m"
+           s ? "[#{v}m" + s + "[0;0m" : "[#{v}m"
            end })
       else
         class_eval(
