55a9ac2015185b0f8cd5a27c7fd0acde17babdef
John Mettraux <jmettraux@gmail.com>
2020-10-13 08:23:58 +0900

    Expand doc/hooks.md [ci skip]

--- doc/hooks.md
+++ doc/hooks.md
@@ -5,5 +5,21 @@ Hooks are pieces of code that are run before or after the flor executor consumes
 
 Hooks are external to flor definitions or executions, they are triggered for any matching message, whatever the execution. [Traps](traps.md) are set by executions and generally terminate when their execution terminates.
 
+## setting a hook programmatically
+
+```ruby
+flor = Flor::Unit.new('envs/test/etc/conf.json')
+# ...
+flor.hooker.add('journal', Flor::Journal)
+```
+
+The [journal](../lib/flor/unit/journal.rb) is a minimal example of hook. It's used in spec to track messages emitted by test executions.
+
+TODO setting a block hook
+
+TODO
+
+## setting a hook via the configuration
+
 TODO
 
--- lib/flor/unit/journal.rb
+++ lib/flor/unit/journal.rb
@@ -1,13 +1,23 @@
 
 module Flor
 
+  # A Journal hook, receives only "consumed" messages and
+  # keeps a copy of all of them.
+  #
+  # Used in specs, do not uses as-is in production, since it simply
+  # grows in memory...
+  #
   class Journal
 
     attr_reader :messages
 
     def initialize(unit)
 
+      # Add a #journal method to the flor unit so that
+      # `unit.journal` yields the message list.
+      #
       unit.singleton_class.instance_eval do
+
         define_method(:journal) do
           @hooker['journal'].messages
         end
@@ -16,11 +26,23 @@ module Flor
       @messages = []
     end
 
+    # Tells the hooker that this hook is only interested in message that
+    # have been "consumed", remember, the hooker passes messages to
+    # hooks before consumption and after consumption. In this case only
+    # consumed messages are passed.
+    #
+    # Other hooks may declare they are only interested in messages
+    # belonging to a certain domain or having a certain tag.
+    # See spec/unit/unit_hooks_spec.rb for more filtering examples.
+    #
     def opts; { consumed: true }; end
 
+    # The method used by the hooker to give consumed messages to this journal
+    #
     def notify(executor, message)
 
       @messages << Flor.dup(message)
+        # stores a deep clone of each consumed message
 
       [] # no new messages
     end
