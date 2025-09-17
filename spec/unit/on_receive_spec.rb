a06531def8d2f897f4ff1256a5f4a27dc8b8e225
John Mettraux <jmettraux@gmail.com>
2025-09-17 09:44:30 +0900

    Add cursor/move/on receive spec

--- spec/unit/on_receive_spec.rb
+++ spec/unit/on_receive_spec.rb
@@ -80,6 +80,58 @@ describe 'Flor unit' do
           '0_0_1_0-4/alpha/break', '0_1/omega/' ]
       )
     end
+
+    it 'can be used inside of cursor (move instead of start:)' do
+
+      class MobyTasker < Flor::StagedBasicTasker
+
+        @@counter = 0
+
+        def on_task
+
+          @@counter += 1
+
+          payload['outcome'] =
+            case attl[1]
+            when 'sierra' then @@counter > 3 ? 'break' : 'alpha'
+            else nil
+            end
+
+          payload['l'] << "#{@@counter}/#{attl[1]}/#{payload['outcome']}"
+
+          reply
+        end
+      end
+
+      @unit.add_tasker('moby', MobyTasker)
+
+      r = @unit.launch(
+        %q{
+          cursor
+            on receive
+              #abort _ if f.outcome == 'abort'
+              break _ if f.outcome == 'break'
+              move to: 'alpha' if f.outcome == 'alpha'
+            move to: 'sierra'
+            moby 'alpha'
+            moby 'sierra'
+          moby 'omega'
+        },
+        payload: { l: [] },
+        wait: true)
+
+      expect(r).to have_terminated_as_point
+
+      expect(
+        r['payload']
+      ).to eq(
+        'ret' => 'omega',
+        'outcome' => nil,
+        'l' => [
+          '1/sierra/alpha', '2/alpha/', '3/sierra/alpha', '4/alpha/',
+          '5/sierra/break', '6/omega/' ]
+      )
+    end
   end
 end
 
